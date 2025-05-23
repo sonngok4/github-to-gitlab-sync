e619b1d3a8dd70d411cc77da06c0f1e19efe7e42
sonngok4 <ngoson2k40501@gmail.com>
2025-05-23 09:52:21 +0700

    update sync

--- sync.sh
+++ sync.sh
@@ -2,17 +2,13 @@
 
 source .env
 
-# Đường dẫn làm việc tạm thời
 WORKDIR="/tmp/repo-sync"
 
-# Dọn dẹp
 rm -rf $WORKDIR
 mkdir -p $WORKDIR
 cd $WORKDIR
 
-# Clone từ GitHub
 git clone --mirror "$GITHUB_REPO"
 cd *.git
 
-# Push sang GitLab
 git push --mirror "$GITLAB_REPO"
